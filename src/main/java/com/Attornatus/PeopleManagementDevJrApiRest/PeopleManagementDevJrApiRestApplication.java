7fb0d6314b052bddf25f76377f8d9b26f024ed2c
Igor Soares Fernandes <igordo.sf@gmail.com>
2023-01-09 19:04:30 -0300

    Projeto commit.

---
+++ src/main/java/com/Attornatus/PeopleManagementDevJrApiRest/PeopleManagementDevJrApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Attornatus.PeopleManagementDevJrApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementDevJrApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementDevJrApiRestApplication.class, args);
+	}
+
+}
