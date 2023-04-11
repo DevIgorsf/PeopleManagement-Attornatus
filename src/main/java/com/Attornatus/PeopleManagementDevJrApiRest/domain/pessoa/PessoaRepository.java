f0131a493ce5516914f8994d70b03c5b62a4e955
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-04-11 18:31:21 -0300

    Inclusão da anotação do repository.

--- src/main/java/com/Attornatus/PeopleManagementDevJrApiRest/domain/pessoa/PessoaRepository.java
+++ src/main/java/com/Attornatus/PeopleManagementDevJrApiRest/domain/pessoa/PessoaRepository.java
@@ -1,10 +1,12 @@
 package com.Attornatus.PeopleManagementDevJrApiRest.domain.pessoa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
     Optional<Pessoa> findByNome(String nome);
 }
